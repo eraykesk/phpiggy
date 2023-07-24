9df2a5f0183056de6d70a6721daabd123441029b
Eray Keskiner <keskinerer@mef.edu.tr>
2023-07-25 02:42:49 +0300

    getting transactions from database and listing them

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use DateTimeZone;
 use Framework\Validator;
 use Framework\Rules\{
     RequiredRule,
@@ -11,7 +12,10 @@ use Framework\Rules\{
     MinRule,
     InRule,
     UrlRule,
-    MatchRule
+    MatchRule,
+    LengthMaxRule,
+    NumericRule,
+    DateFormatRule
 };
 
 class ValidatorService
@@ -28,6 +32,9 @@ class ValidatorService
         $this->validator->add('in', new InRule());
         $this->validator->add('socialMediaUrl', new UrlRule);
         $this->validator->add('match', new MatchRule());
+        $this->validator->add('lengthMax', new LengthMaxRule());
+        $this->validator->add('numeric', new NumericRule());
+        $this->validator->add('dateFormat', new DateFormatRule());
     }
     public function validateRegister(array $formData)
     {
@@ -46,6 +53,14 @@ class ValidatorService
         $this->validator->validate($formData, [
             'email' => ['required', 'email'],
             'password' => ['required']
+        ]);
+    }
+    public function validateTransaction(array $formData)
+    {
+        $this->validator->validate($formData, [
+            'description' => ['required', 'lengthMax:255'],
+            'amount' => ['required', 'numeric'],
+            'date' => ['required', 'dateFormat:Y-m-d']
         ]);
     }
 }
