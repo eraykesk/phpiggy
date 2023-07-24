f358e28d84c32b6f8d56214657c0fffac03e7c6c
Eray Keskiner <keskinerer@mef.edu.tr>
2023-07-24 13:57:34 +0300

    Form validation for register page

--- src/App/functions.php
+++ src/App/functions.php
@@ -1,14 +1,23 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
-function dd(mixed $value) {
+function dd(mixed $value)
+{
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
     die();
 }
 
-function e(mixed $value): string{
+function e(mixed $value): string
+{
     return htmlspecialchars((string) $value);
+}
+
+function redirectTo(string $path)
+{
+    header("Location: {$path}");
+    http_response_code(302);
+    exit;
 }
