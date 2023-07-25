5fa9e27410665eb407c8f53c10e9f640cee72d4f
Eray Keskiner <keskinerer@mef.edu.tr>
2023-07-25 11:24:38 +0300

    pages and page links

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -26,21 +26,31 @@ class TransactionService
         );
     }
 
-    public function getUserTransactions()
+    public function getUserTransactions(int $length, int $offset)
     {
         $searchTerm = addcslashes($_GET['s'] ?? '', '%_');
+        $params = [
+            'user_id' => $_SESSION['user'],
+            'description' => "%{$searchTerm}%"
+        ];
 
         $transactions = $this->db->query(
             "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date 
             FROM transactions 
             WHERE user_id = :user_id
-            AND description LIKE :description",
-            [
-                'user_id' => $_SESSION['user'],
-                'description' => "%{$searchTerm}%"
-            ]
+            AND description LIKE :description
+            LIMIT {$length} OFFSET {$offset}",
+            $params
         )->findAll();
 
-        return $transactions;
+        $transactionCount = $this->db->query(
+            "SELECT COUNT(*)
+            FROM transactions 
+            WHERE user_id = :user_id
+            AND description LIKE :description",
+            $params
+        )->count();
+
+        return [$transactions, $transactionCount];
     }
 }
