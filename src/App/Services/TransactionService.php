8c4511a8a541af602a99ca564ad2b5dae00a03cb
Eray Keskiner <keskinerer@mef.edu.tr>
2023-07-25 10:39:25 +0300

    formatting the date

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -29,7 +29,9 @@ class TransactionService
     public function getUserTransactions()
     {
         $transactions = $this->db->query(
-            "SELECT * FROM transactions WHERE user_id = :user_id",
+            "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date 
+            FROM transactions 
+            WHERE user_id = :user_id",
             ['user_id' => $_SESSION['user']]
         )->findAll();
 
