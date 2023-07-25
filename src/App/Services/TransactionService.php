df5361741e23a3e784b147b9f806e6b086870e75
Eray Keskiner <keskinerer@mef.edu.tr>
2023-07-25 10:48:59 +0300

    filtering transactions

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -28,11 +28,17 @@ class TransactionService
 
     public function getUserTransactions()
     {
+        $searchTerm = $_GET['s'] ?? '';
+
         $transactions = $this->db->query(
             "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date 
             FROM transactions 
-            WHERE user_id = :user_id",
-            ['user_id' => $_SESSION['user']]
+            WHERE user_id = :user_id
+            AND description LIKE :description",
+            [
+                'user_id' => $_SESSION['user'],
+                'description' => "%{$searchTerm}%"
+            ]
         )->findAll();
 
         return $transactions;
